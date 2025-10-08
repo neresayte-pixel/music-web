d458135810d2519d4510a6baa69a2f66579c158c
neresayte-pixel <neresayte@gmail.com>
2025-10-08 15:53:02 +0500

    jkewqj

---
+++ script.js
@@ -0,0 +1,9 @@
+fetch('https://ipapi.co/json/')
+  .then(res => res.json())
+  .then(data => {
+    if (data.country_code !== 'UZ') {
+      // Если не из Узбекистана — перенаправляем
+      window.location.href = 'https://google.com'; // или твоя страница "нет доступа"
+    }
+  })
+  .catch(err => console.error('Ошибка при определении страны:', err));
